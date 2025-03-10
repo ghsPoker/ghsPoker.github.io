604c2a6645eec07539b843fd647c50b1e6a3a97f
m-wie-mischa <mischa.google@gmx.de>
2025-03-10 10:59:57 +0100

    index for cards

--- poker.js
+++ poker.js
@@ -1,4 +1,10 @@
 import { cardDeck, getCardValue } from './modules/deck.mjs';
 
+<<<<<<< Updated upstream
 cardDeck.splice(1)
 console.log(cardDeck)
+=======
+console.log(cardDeck[0])
+console.log(getCardValue(cardDeck[0]))
+console.log(cardDeck.length)
+>>>>>>> Stashed changes
