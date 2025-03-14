845929b8911cc194b560da6d62291e7555919361
Albert04German <albrecht.roehrs@gmail.com>
2025-03-14 20:21:14 +0100

    aua

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
 import { cardDeck, getCardValue, pullCard } from './modules/deck.mjs';
-document.getElementById("pullCard").onclick = function click() {
-    const c0 = document.getElementById('C0');
-    const c1 = document.getElementById('C1');
-    const val = pullCard(deck)
-    document.getElementById("cock").innerHTML = ""
-}
+
 window.onload = function()//called when window gets loaded
 {
-
+    
 };
 function cardSprite(newSprite, cardID)//newSprite = string/path, cardindex = string
 {
