edbdac3e7f14ea5eded68b5864fd84b8f68e750f
Albert04German <albrecht.roehrs@gmail.com>
2025-03-14 19:27:53 +0100

    simple UI

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-document.getElementById("theB").onclick = function click() {
-    document.getElementById('text').innerHTML = "n' Abend"
+import { cardDeck, getCardValue, pullCard } from './modules/deck.mjs';
+document.getElementById("pullCard").onclick = function click() {
+    const c0 = document.getElementById('C0');
+    const c1 = document.getElementById('C1');
+    const val = pullCard(deck)
+    document.getElementById("cock").innerHTML = ""
+}
+window.onload = function()//called when window gets loaded
+{
+
+};
+function cardSprite(newSprite, cardID)//newSprite = string/path, cardindex = string
+{
+    const cardImg = document.getElementById(cardID);
+    cardImg.src = newSprite;
 }
