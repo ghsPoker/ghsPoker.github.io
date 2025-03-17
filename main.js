cd478314ba0b3f579ec87bf6d2812040b9b04be9
Albert04German <albrecht.roehrs@gmail.com>
2025-03-17 19:00:59 +0100

    Animations!

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-import { cardDeck, getCardValue, pullCard, getCardColor } from './modules/deck.mjs';
+//import { cardDeck, getCardValue, pullCard, getCardColor } from './modules/deck.mjs';
 
-/*
+
 window.onload = function() {
     //called when window gets loaded
     
+    
 };
-*/
+
 
 function cardSprite(obj, id = null) {
     let suitDir = "./assets/" + obj.suit + "Background.png"
@@ -16,4 +17,20 @@ function cardSprite(obj, id = null) {
         document.getElementById(id).src = suitDir
     }
 }
-
+function clickedOnCard(obj,i)//basicly just a figet toy
+{
+    obj.classList.add("clickedOnCardAnim");
+    if(i == 0)
+    {
+        const card1 = document.getElementById("card1");
+        card1.classList.remove("clickedOnCardAnim");
+    }else
+    {
+        const card0 = document.getElementById("card0");
+        card0.classList.remove("clickedOnCardAnim");
+    }
+}
+const card0 = document.getElementById('card0');
+const card1 = document.getElementById('card1');
+card0.addEventListener("click",function(){clickedOnCard(card0,0)});
+card1.addEventListener("click",function(){clickedOnCard(card1,1)});
